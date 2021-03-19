dc3b22690d791eb2fb7261f8cd490dab82763cbf
Melissa Chen <myc2@illinois.edu>
2021-03-18 23:42:19 -0500

    Adding frontend and dummy javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("encode-button").addEventListener("click", function() {
+    document.getElementById("encode-output").innerHTML = "Hello World";
+  });
+
+  document.getElementById("decode-button").addEventListener("click", function() {
+    document.getElementById("decode-output").innerHTML = "Hello World";
+  });
