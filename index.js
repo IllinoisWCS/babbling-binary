fcd24af0bdbf5cd8f35991b8012271f1eab76bef
Melissa Chen <myc2@illinois.edu>
2021-03-19 20:33:04 -0500

    guarding for adversarial inputs

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-document.getElementById("encode-button").addEventListener("click", function() {
+document.getElementById("encode-button").addEventListener("click", function () {
     var encoded_message = "";
     var message = document.getElementById("encode-input").value;
 
+    // references: https://stackoverflow.com/questions/94037/convert-character-to-ascii-code-in-javascript
+    for (var i = 0; i < message.length; i++) {
+        if (message[i].charCodeAt(i) > 127) {
+            document.getElementById("encode-output").innerHTML = "sorry, we aren't able to handle non-ascii characters at this time. for an explanation on why, check out the background page!";
+            return;
+        }
+    }
+
     // references:
-    // https://stackoverflow.com/questions/94037/convert-character-to-ascii-code-in-javascript
     // https://stackoverflow.com/questions/9939760/how-do-i-convert-an-integer-to-binary-in-javascript
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
     for (var i = 0; i < message.length; i++) {
@@ -11,17 +18,29 @@ document.getElementById("encode-button").addEventListener("click", function() {
     }
 
     document.getElementById("encode-output").innerHTML = encoded_message;
-  });
+});
 
-document.getElementById("decode-button").addEventListener("click", function() {
+document.getElementById("decode-button").addEventListener("click", function () {
     var decoded_message = "";
     var message = document.getElementById("decode-input").value;
-    
+
+    if (message.length % 8 != 0) {
+        document.getElementById("decode-output").innerHTML = "careful! your message has some extra characters or is missing some characters. i can't decode this.";
+        return;
+    }
+
+    for (var i = 0; i < message.length; i++) {
+        if (decoded_message[i] != 0 && decoded_message[i] != 1) {
+            document.getElementById("decode-output").innerHTML = "careful! your message contains characters that are not 0 or 1. i can't decode this.";
+            return;
+        }
+    }
+
     // references:
     // https://stackoverflow.com/questions/11103487/how-to-convert-binary-representation-of-number-from-string-to-integer-number-in
     // https://www.w3schools.com/jsref/jsref_fromcharcode.asp
     for (var i = 0; i < message.length; i += 8) {
-        decoded_message += String.fromCharCode(parseInt(message.substring(i, i+8), 2));
+        decoded_message += String.fromCharCode(parseInt(message.substring(i, i + 8), 2));
     }
 
     document.getElementById("decode-output").innerHTML = decoded_message;
